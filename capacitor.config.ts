c35a5fea6195bc75fe07794bc9e0ab34afcce229
tobycourtis <toby.courtis@hotmail.com>
2024-06-03 17:54:57 +0100

    🐛 Updated config for live reloading

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.paceconverter.app',
   appName: 'pace-converter',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'http://localhost:8100',
+    cleartext: true
+  }
 };
 
 export default config;
