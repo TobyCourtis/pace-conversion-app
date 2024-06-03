35d3cdc76730edb2416da6e8827a7f174575fe3d
tobycourtis <toby.courtis@hotmail.com>
2024-06-03 12:42:11 +0100

    ✨ Functional history section w/save button

--- src/components/TimeSelector.tsx
+++ src/components/TimeSelector.tsx
@@ -1,9 +1,12 @@
 import React, {useState} from 'react';
-import {IonContent, IonDatetime} from '@ionic/react';
+import {IonButton, IonContent, IonDatetime} from '@ionic/react';
 import './TimeSelector.css';
+import {IonIcon} from '@ionic/react';
+import {arrowDownOutline} from 'ionicons/icons';
 
 const TimeSelector = () => {
     const [selectedTime, setSelectedTime] = useState<string>("05:30");
+    const [history, setHistory] = useState<{ time: string; speed: string }[]>([]);
 
     const handleTimeChange = (e: CustomEvent) => {
         const newTime = e.detail.value!;
@@ -22,6 +25,20 @@ const TimeSelector = () => {
         return (3600 / ((mins * 60) + secs)).toFixed(1).toString();
     }
 
+    const handleLogTime = () => {
+        const speed = convertToSpeed(selectedTime);
+        const newEntry = {time: selectedTime, speed};
+
+        const alreadyExists = history.some(entry => entry.time === newEntry.time);
+
+        if (alreadyExists) {
+            console.log('TODO - show modal saying already added');
+        } else {
+            setHistory(prevHistory => [...prevHistory, newEntry]);
+        }
+    };
+
+
     return (
         <IonContent className="ion-padding no-scroll time-selector-content">
             <div className={'time-selector-datetime-div centre'}>
@@ -33,13 +50,37 @@ const TimeSelector = () => {
                 />
             </div>
 
+            <IonIcon icon={arrowDownOutline} className={'centre'} id={'arrow-down'}/>
+
             <div className={'converted-time-div centre'}>
                 {selectedTime &&
                     <h1>{convertToSpeed(selectedTime)} km/h</h1>
                 }
             </div>
 
-            <button onClick={() => console.log(selectedTime)}>Log the time</button>
+            <div className="history-section">
+                <div className="history-header">
+                    <h2 id={'history-title'}>History</h2>
+                    <IonButton color="medium" onClick={handleLogTime} id={'save-button'} size={'small'}>
+                        Save
+                    </IonButton>
+                </div>
+                <ul>
+                    {history.length > 0 ? (
+                        history.map((entry, index) => (
+                            <li key={index}>
+                                {entry.time} min/km - <strong>{entry.speed} km/h</strong>
+                            </li>
+                        ))
+                    ) : (
+                        <>
+                            <br/>
+                            <p>Press save to add results to history</p>
+                        </>
+                    )}
+                </ul>
+
+            </div>
         </IonContent>
     );
 };
