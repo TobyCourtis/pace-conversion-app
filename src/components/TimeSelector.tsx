a196aca9ba277bc3a388c21b3cdc32cf85ef474a
tobycourtis <toby.courtis@hotmail.com>
2024-06-03 11:35:20 +0100

    ✨ Added v1 pace converter to TimeSelector

--- src/components/TimeSelector.tsx
+++ src/components/TimeSelector.tsx
@@ -3,24 +3,45 @@ import {IonContent, IonDatetime} from '@ionic/react';
 import './TimeSelector.css';
 
 const TimeSelector = () => {
-    const [selectedTime, setSelectedTime] = useState('');
+    const [selectedTime, setSelectedTime] = useState<string>("05:30");
 
-    const handleTimeChange = (e) => {
-        setSelectedTime(e.detail.value);
-        console.log('Selected Time:', console.log(selectedTime));
+    const handleTimeChange = (e: CustomEvent) => {
+        const newTime = e.detail.value!;
+        setSelectedTime(newTime);
     };
+
+    const convertToSpeed = (time: string): string => {
+        if (!time) {
+            return "";
+        }
+
+        const [minsString, secsStr] = time.split(":");
+        const mins: number = parseInt(minsString, 10);
+        const secs: number = parseInt(secsStr, 10);
+
+        return (3600 / ((mins * 60) + secs)).toFixed(1).toString();
+    }
 
     return (
         <IonContent className="ion-padding no-scroll time-selector-content">
-            <div className={'time-selector-datetime-div'}>
+            <div className={'time-selector-datetime-div centre'}>
                 <IonDatetime
                     presentation="time"
                     displayFormat="HH:mm"
                     pickerFormat="HH:mm"
                     onIonChange={handleTimeChange}
                     className="time-selector-datetime"
+                    value={selectedTime}
                 />
             </div>
+
+            <div className={'converted-time-div centre'}>
+                {selectedTime &&
+                    <h1>{convertToSpeed(selectedTime)} km/h</h1>
+                }
+            </div>
+
+            <button onClick={() => console.log(selectedTime)}>Log the time</button>
         </IonContent>
     );
 };
