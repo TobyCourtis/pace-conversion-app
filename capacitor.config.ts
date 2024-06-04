cebcd556be93258bda1dc54db4bb958595bfe32c
tobycourtis <toby.courtis@hotmail.com>
2024-06-04 22:19:35 +0100

    🔧 Change capacitor config to serve static offline files not proxy server

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,16 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type {CapacitorConfig} from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.paceconverter.app',
-  appName: 'pace-converter',
-  webDir: 'dist',
-  server: {
-    url: 'http://localhost:8100',
-    cleartext: true
-  }
+    appId: 'com.paceconverter.app',
+    appName: 'pace-converter',
+    webDir: 'dist'
 };
+
+// add the below to the CapacitorConfig if you want capacitor's web server to act as a proxy to the URL below
+// this is one way of enabling live reloading. See README for more.
+// server: {
+//     url: 'http://192.168.55.166:8100',
+//         cleartext: true
+// }
 
 export default config;
